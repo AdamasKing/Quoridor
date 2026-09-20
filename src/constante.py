4e5ab1080650cdecb92dbd264af5574e5b32fffc
ALEX <barbarasalexandre05@gmail.com>
2026-09-20 22:03:37 +0200

    Mise en place de l'arborescence src et des constantes du jeu

---
+++ src/constante.py
@@ -0,0 +1,9 @@
+# Nombre de cellule par coté
+NB_CELLULE = 9
+# Nombre de mur que peut poser chaque joueur dans la partie
+NB_MUR_PAR_JOUEUR = 10
+# Coordonnées d'apparition des joueurs 1 et 2 en début de partie
+COORDONNEES_SPAWN = [(4, 0), (4, 8)]
+# Position de la case de victoire qui est au centre du plateau
+CASE_VICTOIRE = (4, 4)
+
